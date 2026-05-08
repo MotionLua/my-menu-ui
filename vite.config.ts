6836180b913e639b8ebc6404027608b2c357e270
MotionLua <tony03142006@gmail.com>
2026-05-08 15:32:12 -0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  server: {
+    hmr: {
+        overlay: false,
+    }
+  },
+  build: {
+    outDir: 'build',
+  },
+});
